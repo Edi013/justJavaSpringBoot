83d1a9f6a1f863e41f091f938a2f5e05440bd238
Edi013 <edymare97@yahoo.com>
2025-12-08 17:30:18 +0200

    Ch12 + Ex1 + Ex 2 inceput

---
+++ DataSourcesCh12/DataSourcesEx2/src/main/java/com/rest/RestServices/Data.java
@@ -0,0 +1,11 @@
+package com.rest.RestServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data {
+	public static void main(String[] args) {
+		SpringApplication.run(Data.class, args);
+	}
+}
---
+++ DataSourcesCh12/DataSourcesEx2/src/main/java/com/rest/RestServices/controllers/PurchaseController.java
@@ -0,0 +1,28 @@
+package com.rest.RestServices.controllers;
+
+import com.rest.RestServices.models.Purchase;
+import com.rest.RestServices.repositories.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
