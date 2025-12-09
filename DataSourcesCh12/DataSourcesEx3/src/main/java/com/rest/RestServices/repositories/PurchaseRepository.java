30bed53e896182eb72cd47750819266f891e7d68
Edi013 <edymare97@yahoo.com>
2025-12-09 12:24:12 +0200

    new project with PostgreSQL instead of H2

---
+++ DataSourcesCh12/DataSourcesEx3/src/main/java/com/rest/RestServices/repositories/PurchaseRepository.java
@@ -0,0 +1,42 @@
+package com.rest.RestServices.repositories;
+
+import com.rest.RestServices.models.Purchase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PurchaseRepository {
+    private final JdbcTemplate jdbc;
+
+    public PurchaseRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public void storePurchase(Purchase purchase) {
+        String sql =
+        "INSERT INTO purchase VALUES (? , ?, ?)";
+
+        int random = (int)Math.ceil(Math.random());
+        jdbc.update(sql,
+                random,
+                purchase.getProduct(),
+                purchase.getPrice());
+    }
+
+    public List<Purchase> findAllPurchases() {
+        String sql = "SELECT * FROM purchase";
+
+        RowMapper<Purchase> purchaseRowMapper = (r, i) -> {
+            Purchase rowObject = new Purchase();
+            rowObject.setId(r.getInt("id"));
+            rowObject.setProduct(r.getString("product"));
+            rowObject.setPrice(r.getBigDecimal("price"));
+            return rowObject;
+        };
+
+        return jdbc.query(sql, purchaseRowMapper);
+    }
+}
